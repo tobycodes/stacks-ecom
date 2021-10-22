c18db1a064e28b3982e7655748e1b5dcb6daa20b
tobycodes <davidolubusoye@gmail.com>
2021-10-22 16:13:51 +0100

    adding clarity contracts

---
+++ src/hooks/useUser.js
@@ -0,0 +1,7 @@
+import { userSession } from "../api/auth";
+
+export function useUser() {
+  if (userSession.isUserSignedIn()) return userSession.loadUserData();
+
+  return {};
+}
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,7 @@
 import { Box, Text, Flex } from "@stacks/ui";
+import { useHistory } from "react-router-dom";
 import { useCart } from "../context/Cart";
+import { useUser } from "../hooks/useUser";
 
 import List from "../components/common/List";
 import Button from "../components/common/Button";
@@ -7,6 +9,10 @@ import CartItem from "../components/shop/CartItem";
 
 const Cart = () => {
   const [cart, , { loading }] = useCart();
+  const user = useUser();
+  const history = useHistory();
+
+  console.log({ user });
 
   if (loading)
     return (
@@ -14,6 +20,17 @@ const Cart = () => {
         Loading...
       </Text>
     );
+
+  if (cart.products?.length === 0) {
+    return (
+      <Flex p="4rem" align="center" direction="column">
+        <Text mb="2rem">You do not have any items in your cart.</Text>
+        <Button py="1.5rem" onClick={() => history.push("/all-products")}>
+          Continue Shopping
+        </Button>
+      </Flex>
+    );
+  }
 
   return (
     <Box px={["2rem", "3rem", "4rem"]} maxW="100rem" m="0 auto" mb="6rem">
